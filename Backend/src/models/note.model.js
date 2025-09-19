5dace5d5f0737468986d5aabc1b1eeb71e6c741b
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-19 15:59:46 +0530

    auth :)

---
+++ Backend/src/models/note.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const NoteSchema = new mongoose.Schema({
+    id: { type: String},
+    title: { type: String, required: true, trim: true},
+    discription: { type: String , required: true, trim: true},
+    mediaUpload: { type: String, trim: true, }
+}, { timestamps: true})
+
+export default mongoose.model("Note" , NoteSchema)
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+const UserSchema = new mongoose.Schema({
+    id:{ type: String, },
+    username: { type: String, required: true, trim: true, lowercase: true},
+    avatar: { type: {
+        url: String,
+        localpath: String
+    },default: { url: "https://placehold.co/600x400", localpath: ""}},
+    email: { type: String, required: true, trim: true, lowercase: true, },
+    password: { type: String , required: true, trim: true,},
+    isVarified: { type: Boolean, default: false },
+    refreshToken:{ type: String },
+    forgetPasswordToken: { type: String},
+    forgetPasswordTokenExpiry: { type: Date},
+    emailVarificationToken: { type: String},
+    emailVarificationTokenExpiry:{ type: Date},
+
+},{ timestamps: true})
+
+UserSchema.pre("save" , async function (next) {
+    if(this.isModified('password')) {
+        const salt_round = bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password , salt_round);
+    }
+    next()
+})
+
+UserSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password , this.password)
+}
+
+export default mongoose.model("User" , UserSchema)
