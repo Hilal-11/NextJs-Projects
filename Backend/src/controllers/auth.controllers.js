fb557d81c23e388c579245786ac69cc79e30f58c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-19 16:00:19 +0530

    auth :)

---
+++ Backend/src/controllers/auth.controllers.js
@@ -0,0 +1,60 @@
+import User from "../models/User.model";
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const signUp = async () => {
+    try{
+
+    }catch(error) {
+
+    }
+}
+
+const login = async () => {
+    try{
+
+    }catch(error) {
+        
+    }
+}
+const varifyEmail = async () => {
+    try{
+
+    }catch(error) {
+        
+    }
+}
+const logout = async () => {
+    try{
+
+    }catch(error) {
+        
+    }
+}
+
+const forgetPassword = async () => {
+    try{
+
+    }catch(error) {
+        
+    }
+}
+
+const resetPassword = async () => {
+    try{
+
+    }catch(error) {
+        
+    }
+}
+
+const userProfile = async () => {
+    try{
+
+    }catch(error) {
+        
+    }
+}
+
+export { signUp , login , varifyEmail , logout, forgetPassword , resetPassword , userProfile }
