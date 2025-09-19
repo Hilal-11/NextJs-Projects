1718c1ea0f41e404b14410281f60b0ec17a69448
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-20 03:24:25 +0530

    Controlers

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -133,19 +133,104 @@ const login = async () => {
 }
 
 const sendVarificationOTP = async () => {
+    const { id } = req.body;
+    if(!id) {
+        return res.status().json({
+
+        })
+    }
     try{
+        const user = await User.findById({ id });
+        if(!user.isVarified){
+            return res.status().json({
+                success: false,
+                message: "User already varified"
+            })
+        }
+
+        // Generate 6-Digit OTP
+        const OTP = String(Math.floor(100000 + Math.random() * 900000));
+        user.varifyOtp = OTP
+        user.varifyOtpExpireAt = Date.now() + 15 * 60  * 1000 //15 minutes
+        
+        
+        await user.save()
+
+        // Send OTP on Emial
+        const mailOptions = {
+            from: process.env.SENDER_MAIL,
+            to: user.email,
+            subject: `User varification OTP,  use thia OTP ${OTP} to varify your account`,
+            text: "Welcome to Keep",
+            html: `Hi ${user.username} welcome in Keep.com`
+
+        }
+        transporter.sendMail(mailOptions)
+            .then((info) => {
+                console.log("Otp is sent successfully", info)
+            }).catch((error) => {
+                console.log("Failed to sent OPT", error.message)
+            })
+
+        // return success respons
+        res.status(200).json({
+            success: true,
+            message: "Varifivartion OTP sent successfully",
+        })
+
+    }catch(error) {
+        console.log(error.message)
+        return res.status(400).json({
+            success: false,
+            message: "Failed to varify account"
+        })
+    }
+}
+
+const varifyEmail = async (req , res) => {
+    const { id , OTP } = user.body;
+    if(!id || !OTP) {
+        return res.status(400).json({
+            success: false,
+            message: "Incorrect user and OTP"
+        })
+    }
+    try{    
+        const user = await User.findById({ id })
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "user is not exists "
+            })
+        }
+        //  VARIFY THE OTP
+        if(user.varifyOtp === '' || user.varifyOtp !== OTP) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid OTP"
+            })
+        }
+
+        if(user.varifyOtpExpireAt < Date.now()) {
+            return res.status(401).json({
+                success: false,
+                message: "OTP Expired"
+            })
+        }
+
+        user.varifyEmail = true;
+        user.varifyOtp = '',
+        user.varifyOtpExpireAt = 0
+
+        await user.save()
+
+        return res.status(200).json({ success: true , message: "Email Varified Successfully"})
 
     }catch(error) {
         
     }
 }
-const varifyEmail = async () => {
-    try{
 
-    }catch(error) {
-        
-    }
-}
 const logout = async (req , res) => {
     try{
         res.clearCookie("cookie" , {
@@ -167,28 +252,135 @@ const logout = async (req , res) => {
     }
 }
 
-const forgetPassword = async () => {
+const passwordResetOTP = async (req , res) => {
+    const { email } = req.body
+    if(!email) {
+        return res.status(400).json({
+            success: false,
+            message: "user not found while trying to forgetting password"
+        })
+    }
     try{
+        const user = await User.findById(email)
+        if(!user) {
+            res.status(400).json({
+                success: true,
+                message: "Failed to forget password"
+            })
+        }
+
+        // Generate OTP
+        const OTP = String(Math.floor(100000 + Math.random() * 900000));
+
+        const mailOptions = {
+            from: process.env.SENDER_MAIL,
+            to: user.email,
+            subject: `Reset password OTP,  use this OTP ${OTP} to Reset your password`,
+            text: "Welcome to Keep",
+            html: `Hi ${user.username} welcome in Keep.com`
+        }
+        transporter.sendMail(mailOptions)
+            .then((info) => {
+                console.log("Reset password OTP sent done ", info)
+            })
+            .catch((error) => {
+                console.log("Failed to send reset OTP",error.message);
+            })
+
+        user.resetPasswordOTP = OTP;
+        user.resetPasswordOTPExpiry = Date.now() + 15 * 60 * 1000;
+        await user.save()
+
+        res.status(200).json({
+            success: true,
+            message: "forget password OTP sent"
+        })
 
     }catch(error) {
-        
+         return res.status(401).json({
+            success: false,
+            message: "Failed to send reset password otp"
+        })
     }
 }
 
-const resetPassword = async () => {
+const resetPassword = async (req , res) => {
+    const { email ,OTP ,  newPassword } = req.body;
+    if(!email || !OTP || !newPassword) {
+        res.status(400).json({
+            success: false,
+            message: "invalid cradentials"
+         })
+    }
     try{
 
-    }catch(error) {
+        const user = await User.findOne(email)
+        if(!user) {
+            res.status(401).json({
+            success: false,
+            message: "user not exists, incorrect email"
+         })
+        }
+
+        if(user.resetPasswordOTP === '' || user.resetPasswordOTP !== OTP) {
+            res.status(400).json({
+                success: false,
+                message: "Invalid reset password OTP"
+            })
+        }
+        if(user.resetPasswordOTPExpiry < Date.now()){
+            res.status(400).json({
+                success: false,
+                message: "Reset Password OTP has been expired"
+            })
+        }
+
+        user.password = newPassword;
+        user.resetPasswordOTP = '';
+        user.resetPasswordOTPExpiry = 0
+
+        res.status(200).json({
+            success: true,
+            message: "Password reset successfully"
+        })
         
+        
+    }catch(error) {
+         return res.status(401).json({
+            success: false,
+            message: "Can't reset password"
+        })
     }
 }
 
-const userProfile = async () => {
+const userProfile = async (req , res) => {
+    const { id } = req.body
+    if(!id) {
+       return res.status(400).json({
+            success: false,
+            message: "invalid user"
+        }) 
+    }
     try{
+        const user = await User.findById(id)
+        if(!user) {
+            return res.status(401).json({
+                success: false,
+                message: "User not found"
+            }) 
+        }
 
+        res.status(200).json({
+            success: true,
+            response: user,
+            message: "User Profile found"
+        })
     }catch(error) {
-        
+        return res.status(401).json({
+            success: false,
+            message: "Can't found user"
+        })
     }
 }
 
-export { signUp , login , varifyEmail , logout, forgetPassword , resetPassword , userProfile }
+export { signUp , login , sendVarificationOTP , varifyEmail , logout, passwordResetOTP , resetPassword , userProfile }
