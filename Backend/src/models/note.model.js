d91452c2d615d93405d8c8a5007e1331c2c6de53
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-20 15:21:39 +0530

    ...

--- Backend/src/models/note.model.js
+++ Backend/src/models/note.model.js
@@ -4,6 +4,7 @@ const NoteSchema = new mongoose.Schema({
     id: { type: String},
     title: { type: String, required: true, trim: true},
     discription: { type: String , required: true, trim: true},
+    color: { type: String , trim: true , default: '#ffffff'},
     mediaUpload: { type: String, trim: true, }
 }, { timestamps: true})
 
