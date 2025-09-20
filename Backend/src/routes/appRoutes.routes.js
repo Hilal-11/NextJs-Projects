95759b8d3e47ed797f2c957314cdcb1a8eeb2870
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-20 15:22:02 +0530

    ..

--- Backend/src/routes/appRoutes.routes.js
+++ Backend/src/routes/appRoutes.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const appRouter = express.Router();
 
-appRouter.get("/getNotes" , );
-appRouter.post("/createNote" , )
+import { createNote , getNotes } from "../controllers/note.controller";
+
+appRouter.get("/getNotes" , getNotes);
+appRouter.post("/createNote" , createNote)
 
 
 export default appRouter;
