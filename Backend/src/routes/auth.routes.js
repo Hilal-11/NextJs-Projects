30a678803ad1f2984f6b7b325d3327e1814b1171
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-20 15:20:53 +0530

    Backend authentication is complete

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,4 +1,21 @@
-import express from 'express'
+import express, { response } from 'express'
 const authRoutes = express.Router();
+
+
+import { signUp , login , varifyEmail , sendVarificationOTP , passwordResetOTP , resetPassword , logout , userProfile , isAuthenticate} from '../controllers/auth.controllers';
+
+
+authRoutes.post("/signup" , signUp)
+authRoutes.post("/login" , login)
+authRoutes.get("/authenticate" , isAuthenticate)
+authRoutes.get("/sendVarificationOTP" , sendVarificationOTP)
+authRoutes.post("/varifyEmail" , varifyEmail)
+authRoutes.get("/passwordResetOTP" , passwordResetOTP)
+authRoutes.post("/resetPassword" , resetPassword)
+authRoutes.get("/logout" , logout);
+authRoutes.get("/profile" , userProfile)
+
+
+
 
 export default authRoutes
