4d437860118953439b8aab5645f4316b6c534358
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-21 06:59:03 +0530

    Complete validation using express-validator

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -4,18 +4,19 @@ const authRoutes = express.Router();
 
 import { signUp , login , varifyEmail , sendVarificationOTP , passwordResetOTP , resetPassword , logout , userProfile , authenticate} from '../controllers/auth.controller.js';
 import auth from '../middlewares/auth.moddleware.js';
+import validator from '../middlewares/validator.middleware.js';
+import { registrationValidator , loginValidator , verifyEmailOTPValidator, resetPasswordOTPValidator } from '../validator/vaidation.js';
 
-authRoutes.post("/signup" , signUp)
-authRoutes.post("/login" , login)
+
+authRoutes.post("/signup" , registrationValidator(), validator , signUp)
+authRoutes.post("/login" ,loginValidator(), validator , login)
 authRoutes.get("/authenticate" , auth , authenticate)
-authRoutes.get("/sendVarificationOTP" ,auth , sendVarificationOTP)
+authRoutes.get("/sendVarificationOTP", verifyEmailOTPValidator(), validator ,auth , sendVarificationOTP)
 authRoutes.post("/varifyEmail" ,  varifyEmail)
-authRoutes.get("/passwordResetOTP" , auth , passwordResetOTP)
+authRoutes.get("/passwordResetOTP" , resetPasswordOTPValidator(), validator , auth , passwordResetOTP)
 authRoutes.post("/resetPassword" ,auth ,  resetPassword)
 authRoutes.get("/logout" , auth , logout);
 authRoutes.get("/profile" , auth , userProfile)
-
-
 
 
 export default authRoutes
